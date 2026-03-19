b3fc50915539aa4fafe213f784637debb770f089
iron-prog <dt915725@gmail.com>
2026-03-19 10:29:26 +0530

    PICARD-2496 Implement unified conflict handling strategy
    - Replace boolean options with move_conflict_strategy
    - Use radio buttons for mutually exclusive UI
    - Apply consistent behavior across file operations
    - Remove legacy options

--- picard/ui/options/renaming.py
+++ picard/ui/options/renaming.py
@@ -78,6 +78,7 @@ class RenamingOptionsPage(OptionsPage):
         ('move_overwrite_existing_files', ['move_overwrite_existing_files']),
         ('move_additional_files', ['move_additional_files']),
         ('move_additional_files_pattern', ['move_additional_files_pattern']),
+        ('move_conflict_strategy', ['move_conflict_strategy']),
         ('delete_empty_dirs', ['delete_empty_dirs']),
         ('rename_files', ['rename_files']),
         ('selected_file_naming_script_id', ['naming_script_selector']),
@@ -250,6 +251,10 @@ class RenamingOptionsPage(OptionsPage):
         self.ui.move_files_to.setCursorPosition(0)
         self.ui.move_additional_files.setChecked(config.setting['move_additional_files'])
         self.ui.move_additional_files_pattern.setText(config.setting['move_additional_files_pattern'])
+        strategy = config.setting['move_conflict_strategy']
+        self.ui.move_conflict_skip.setChecked(strategy == "skip")
+        self.ui.move_conflict_rename.setChecked(strategy == "rename")
+        self.ui.move_conflict_overwrite.setChecked(strategy == "overwrite")
         self.ui.delete_empty_dirs.setChecked(config.setting['delete_empty_dirs'])
         self.ui.move_overwrite_existing_files.setChecked(config.setting['move_overwrite_existing_files'])
         self.naming_scripts = config.setting['file_renaming_scripts']
@@ -286,8 +291,13 @@ class RenamingOptionsPage(OptionsPage):
         config.setting['move_files_to'] = os.path.normpath(self.ui.move_files_to.text())
         config.setting['move_additional_files'] = self.ui.move_additional_files.isChecked()
         config.setting['move_additional_files_pattern'] = self.ui.move_additional_files_pattern.text()
+        if self.ui.move_conflict_skip.isChecked():
+            config.setting['move_conflict_strategy'] = "skip"
+        elif self.ui.move_conflict_rename.isChecked():
+            config.setting['move_conflict_strategy'] = "rename"
+        elif self.ui.move_conflict_overwrite.isChecked():
+            config.setting['move_conflict_strategy'] = "overwrite"
         config.setting['delete_empty_dirs'] = self.ui.delete_empty_dirs.isChecked()
-        config.setting['move_overwrite_existing_files'] = self.ui.move_overwrite_existing_files.isChecked()
         config.setting['selected_file_naming_script_id'] = self.selected_naming_script_id
 
     def display_error(self, error):
